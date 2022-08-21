270a04101289f432f5b838ca5dd841ac15d09f52
Eduardo Fernando Dias <dudu.dias74@gmail.com>
2022-08-21 15:37:34 -0300

    Alteração nas telas

---
+++ EduardoEduardoFabioIdevanPhaulo/ProblemDetail/Controller/ProblemDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ProblemDetailViewController.swift
+//  EduardoEduardoFabioIdevanPhaulo
+//
+//  Created by Eduardo Fernando Dias on 21/08/22.
+//
+
+import UIKit
+
+class ProblemDetailViewController: UIViewController {
+
+    @IBOutlet weak var LabelProblem: UILabel!
+    
+    @IBOutlet var LabelAddress: UIView!
+    
+    
+    @IBOutlet weak var LabelData: UILabel!
+    
+    @IBOutlet weak var LabelDescription: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
